9c4989eae038a2ede6b8c3b293405b0b715b2fea
tksmaru <tksmaru@gmail.com>
2017-08-27 08:10:03 +0900

    Add sample web project with springboot.

---
+++ sample/src/main/java/com/camobile/camflake/sample/SampleApplication.java
@@ -0,0 +1,12 @@
+package com.camobile.camflake.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+}
